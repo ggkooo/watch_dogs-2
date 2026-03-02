15868c1888698f22d30900b94e3e393b95b1b339
Giordano Bruno Biasi Berwig <giordano.berwig@sou.unijui.edu.br>
2026-03-02 09:52:41 -0300

    feat: created MaterialIcon component

---
+++ src/components/ui/MaterialIcon/index.tsx
@@ -0,0 +1,8 @@
+type MaterialIconProps = {
+  name: string
+  className?: string
+}
+
+export function MaterialIcon({ name, className = '' }: MaterialIconProps) {
+  return <span className={`material-icons ${className}`.trim()}>{name}</span>
+}
